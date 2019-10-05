f5f57018373a3409e34bffecb070020a75fcea35
Cameron Martens <camerondm9@yahoo.ca>
2019-10-04 23:42:03 -0500

    Use SVG for output image (much faster), improve plot quality

--- Simulation/distance.py
+++ Simulation/distance.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib
-matplotlib.use('agg') #Make pyplot happy
+matplotlib.use('svg') #Make pyplot happy
 import matplotlib.pyplot as plt
 import sim
 
@@ -16,12 +16,12 @@ n	= 0.000_000_001
 sample_rate		= 4*M
 sample_bits		= 8
 center_freq		= 400*K
-chirp_freq		= 0
+chirp_freq		= 40*K
 pulse_time		= 300*u
 
-signal_delay	= 100*u
-signal_to_noise	= 0.1
-corr_threshold	= 0.5
+delay_time		= 40*m
+signal_to_noise	= 1
+corr_threshold	= 180
 
 speed_of_sound	= 343
 
@@ -33,7 +33,7 @@ sample_bits_s = sim.util.ensure_iterable(sample_bits)
 center_freq_s = sim.util.ensure_iterable(center_freq)
 chirp_freq_s = sim.util.ensure_iterable(chirp_freq)
 pulse_time_s = sim.util.ensure_iterable(pulse_time)
-signal_delay_s = sim.util.ensure_iterable(signal_delay)
+delay_time_s = sim.util.ensure_iterable(delay_time)
 signal_to_noise_s = sim.util.ensure_iterable(signal_to_noise)
 corr_threshold_s = sim.util.ensure_iterable(corr_threshold)
 speed_of_sound_s = sim.util.ensure_iterable(speed_of_sound)
@@ -47,27 +47,34 @@ for sample_rate in sample_rate_s:
 		for chirp_freq in chirp_freq_s:
 			for pulse_time in pulse_time_s:
 				#Generate reference signal (for transmission)
-				reference_signal = sim.signal.make_chirp(sample_rate, center_freq, chirp_freq, pulse_time)
+				pulse_samples = round(pulse_time * sample_rate)
+				reference_signal = sim.signal.make_chirp(center_freq, chirp_freq, pulse_time, sample_rate)
 				for sample_bits in sample_bits_s:
 					#Quantize reference signal (for correlation)
 					reference_signal_quantized = sim.signal.quantize(reference_signal, sample_bits)
-					for signal_delay in signal_delay_s:
+					for delay_time in delay_time_s:
 						#Make delayed signal (simulating transmission and travel through air)
-						delayed_signal = np.concatenate((sim.signal.make_silence(sample_rate, signal_delay), reference_signal, sim.signal.make_silence(sample_rate, pulse_time)))
+						delay_samples = round(delay_time * sample_rate)
+						delayed_signal = np.concatenate((sim.signal.make_silence(delay_samples), reference_signal, sim.signal.make_silence(pulse_time, sample_rate)))
 						for speed_of_sound in speed_of_sound_s:
 							for signal_to_noise in signal_to_noise_s:
 								for _ in range(simulate_runs):
 									#Add simulated noise from environment
 									noisy_signal = sim.signal.quantize(sim.signal.add_noise(delayed_signal, signal_to_noise), sample_bits)
 									for corr_threshold in corr_threshold_s:
-										correlation = sim.signal.correlate(reference_signal, noisy_signal)
-										r = sim.signal.peak_index(correlation, corr_threshold, sample_rate)
-										fig = plt.figure()
-										plt.plot(np.concatenate((np.zeros(len(reference_signal)), correlation)))
-										plt.plot(noisy_signal)
-										plt.plot(reference_signal_quantized)
-										#TODO: Improve plot quality/usefulness
-										plt.savefig('test.png')
+										correlation = sim.signal.correlate(noisy_signal, reference_signal)
+										peak, peak_samples, peak_avg, peak_max = sim.signal.peak_index(correlation, corr_threshold, sample_rate)
+										fig = plt.figure(figsize=(64, 48))
+										plt.margins(0.002)
+										plt.plot(correlation, 'm', linewidth=0.3)
+										plt.plot(noisy_signal, 'y', linewidth=0.3)
+										plt.plot(reference_signal_quantized, 'b', linewidth=0.3)
+										plt.axvline(pulse_samples, color='b', linewidth=0.3)
+										plt.axvline(delay_samples, color='y', linewidth=0.3)
+										plt.axvline(delay_samples + pulse_samples, color='y', linewidth=0.3)
+										plt.axvline(peak * sample_rate, color='m', linewidth=0.3)
+										plt.axhline(corr_threshold, color='m', linewidth=0.3)
+										plt.savefig('test.svg', dpi=500, bbox_inches='tight')
 										plt.close(fig)
 										params = (
 											sample_rate,
@@ -75,11 +82,17 @@ for sample_rate in sample_rate_s:
 											center_freq,
 											chirp_freq,
 											pulse_time,
-											signal_delay,
+											delay_time,
 											signal_to_noise,
 											corr_threshold,
 											speed_of_sound,
 											simulate_runs,
+										)
+										r = (
+											peak,
+											peak_samples,
+											peak_avg,
+											peak_max,
 										)
 										if params in result:
 											result[params].append(r)
--- Simulation/sim/signal.py
+++ Simulation/sim/signal.py
@@ -1,14 +1,14 @@
 import numpy as np
 import math
 
-def make_silence(sample_rate, duration):
+def make_silence(duration, sample_rate = 1):
 	"Generates a sequence of zeros"
-	return np.zeros(round(sample_rate * duration))
+	return np.zeros(round(duration * sample_rate))
 
-def make_chirp(sample_rate, center_freq, chirp_freq, duration):
+def make_chirp(center_freq, chirp_freq, duration, sample_rate):
 	"Generates a chirp signal around a center frequency"
 	min_freq = center_freq - chirp_freq
-	time = np.linspace(0, duration, round(sample_rate * duration), False)
+	time = np.linspace(0, duration, round(duration * sample_rate), False)
 	return np.sin(2 * math.pi * (min_freq + chirp_freq / duration * time) * time)
 
 def quantize(signal, bits):
@@ -24,7 +24,7 @@ def add_noise(signal, signal_to_noise_ratio):
 
 def correlate(signal_a, signal_b):
 	"Returns the correlation signal of two input signals"
-	return np.correlate(signal_a, signal_b)
+	return np.correlate(signal_a, signal_b, mode='valid')
 
 def peak_index(signal, threshold, sample_rate = 1):
 	"Computes the weighted average location of the peak amplitude in the signal"
@@ -36,4 +36,4 @@ def peak_index(signal, threshold, sample_rate = 1):
 #print(np.nonzero(np.diff(np.sign(signal))))
 
 
-print(peak_index(np.asarray([0, -1, -0.2, 0.5, 0.9, 1, 1, 0.9, 0.5, -0.2, -1, 0]), 0.8))
+#print(peak_index(np.asarray([0, -1, -0.2, 0.5, 0.9, 1, 1, 0.9, 0.5, -0.2, -1, 0]), 0.8))
