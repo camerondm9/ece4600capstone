0701f933eb56a5387c2f4a70ef9ea757687ae4d5
Hillary Kroeker <hillaryjkroeker@gmail.com>
2020-03-14 15:31:25 -0500

    Add updated GUI

--- GUI/main.py
+++ GUI/main.py
@@ -1,62 +1,166 @@
-import kivy
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
+# GUI for CAPSTONE
+
+import serial
+import time
+import pygame
+import tkinter as tk
+from tkinter import *
+
+# start up the program
+print("Start")
+
+"""Create Window"""
+root = tk.Tk()
+
+"""Edit Window"""
+root.title("High Precision Ultrasonic Local Positioning System")
+root.geometry("900x600")
+root.configure(background="lightgrey")
+
+frame = tk.Frame(root, bg="white")
+frame.place(relx=0.05, rely=0.05, relwidth=0.45, relheight=0.9)
+
+text_1 = Label(frame,
+               text="TOWER LOCATION COORDINATES",
+               bd=1,
+               font="Times 16",
+               height=7,
+               anchor=CENTER
+               )
+text_1.pack()
+
+text_2 = Label(frame,
+               text="Tower 1:",
+               bd=1,
+               font="Times 16",
+               height=3,
+               width=40,
+               anchor=W
+               )
+text_2.pack()
+
+text_3 = Label(frame,
+               text="Tower 2:",
+               bd=1,
+               font="Times 16",
+               height=3,
+               width=40,
+               anchor=W
+               )
+text_3.pack()
+
+text_4 = Label(frame,
+               text="Tower 3:",
+               bd=1,
+               font="Times 16",
+               height=3,
+               width=40,
+               anchor=W
+               )
+text_4.pack()
+
+text_5 = Label(frame,
+               text="Tower 4:",
+               bd=1,
+               font="Times 16",
+               height=3,
+               width=40,
+               anchor=W
+               )
+text_5.pack()
+
+text_6 = Label(frame,
+               text="Tower 5:",
+               bd=1,
+               font="Times 16",
+               height=3,
+               width=40,
+               anchor=W
+               )
+text_6.pack()
 
 
-class MyGrid(GridLayout):
-    def __init__(self, **kwargs):
-        super(MyGrid, self).__init__(**kwargs)
-
-        self.cols = 1
-
-        self.inside = GridLayout()
-        self.inside.cols = 2
-
-        self.inside.add_widget(Label(text="Tower 1: "))
-        self.tower1 = TextInput(multiline=False)
-        self.inside.add_widget(self.tower1)
-
-        self.inside.add_widget(Label(text="Tower 2: "))
-        self.tower2= TextInput(multiline=False)
-        self.inside.add_widget(self.tower2)
-
-        self.inside.add_widget(Label(text="Tower 3: "))
-        self.tower3 = TextInput(multiline=False)
-        self.inside.add_widget(self.tower3)
-
-        self.inside.add_widget(Label(text="Tower 4: "))
-        self.tower4 = TextInput(multiline=False)
-        self.inside.add_widget(self.tower4)
-
-        self.inside.add_widget(Label(text="Tower 5: "))
-        self.tower5 = TextInput(multiline=False)
-        self.inside.add_widget(self.tower5)
-
-        self.add_widget(self.inside)
-
-        self.submit = Button(text="Submit", font_size=40)
-        self.submit.bind(on_press=self.pressed)
-        self.add_widget(self.submit)
-
-    def pressed(self, instance):
-        tower1 = self.tower1.text
-        tower2 = self.tower2.text
-        tower3 = self.tower3.text
-        tower4 = self.tower4.text
-        tower5 = self.tower5.text
-
-        print("Tower 1:", tower1, "Tower 2:", tower2, "Tower 3:", tower3, "Tower 3:", tower4, "Tower 3:", tower5)
-        self.tower1.text = ""
-        self.tower2.text = ""
-        self.tower3.text = ""
-
-class MyApp(App):
-    def build(self):
-        return MyGrid()
+"""Action Event"""
+def pause_function():
+    global music_mode
+    pygame.mixer.music.pause()
+    music_mode = False
+    return music_mode
 
 
-if __name__ == "__main__":
-    MyApp().run()
+def unpause_function():
+    global music_mode
+    pygame.mixer.music.unpause()
+    music_mode = True
+    return music_mode
+
+
+def calibrate_function():
+    #input_data = serialPort.readline()
+    header_1 = Label(frame, text="---CALIBRATION PHASE---", font="Times 15", pady=5)
+    header_1.pack()
+
+    text_2 = Label(frame, text="Hold arm in place for 5 seconds \n", font="Times 15", pady=5)
+    text_2.pack()
+
+    del_total = 0
+
+    for i in range(10):
+        global del_threshold
+        #input_data = serialPort.readline()
+        #data = input_data.decode('utf-8')
+       # split_data = data.split(',')
+        #del_data = float(split_data[0])
+
+        #print("del_data = %s" % del_data)
+
+       # del_total += del_data
+
+        time.sleep(0.5)
+
+    del_threshold = del_total/10
+
+    text_2 = Label(frame, text="Calibration Completed \n"
+                               "Press 'Start Music'", font="Times 15", pady=5)
+    text_2.pack()
+
+    print("del_threshold=%s" % del_threshold)
+
+    return del_threshold
+
+
+def start_function():
+    pygame.init()
+    pygame.mixer.init()
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play(-1)
+    music_mode = True
+    volume_mode = True
+    last_time = time.time()
+    running = True
+
+    while running:
+        #input_data = serialPort.readline()
+       # data = input_data.decode('utf-8')
+       # split_data = data.split(',')
+       # del_data = float(split_data[0])
+        #print("del data = %s" % del_data)
+
+        if time.time() - last_time > 3:
+          #  if del_data > del_threshold:
+                if music_mode:
+                    pause_function()
+                    music_mode = False
+                    last_time = time.time()
+                else:
+                    unpause_function()
+                    music_mode = True
+                    last_time = time.time()
+
+
+"""Register Buttons"""
+loc_button = tk.Button(root, text="Refresh Location", fg="black", bg="grey", command=calibrate_function)
+loc_button.place(relx=0.15, rely=0.85, relwidth=0.25, relheight=0.05)
+
+"""Activate"""
+root.mainloop()
